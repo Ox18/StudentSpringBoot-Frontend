02a67586aa2c24fa7882e6578aa8f4bb34a4df67
Alex <wilmer.delgadoalama@gmail.com>
2021-10-28 18:40:03 -0500

    add student content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AppBar from "./components/AppBar";
+import Student from "./components/Student";
 
 function App() {
   return (
     <div className="App">
       <AppBar />
+      <Student />
     </div>
   );
 }
